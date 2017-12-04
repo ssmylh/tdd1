eb348e8faae69dbca96ba6f685d25eddbf4dbd78
ssmylh <ssmylh@gmail.com>
2017-12-04 21:36:47 +0900

    Fix typo

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class MoneyTest {
     @Test
-    void testMultiplicateion() {
+    void testMultiplication() {
         Dollar five = new Dollar(5);
         five.times(2);
         assertEquals(10, five.amount);
