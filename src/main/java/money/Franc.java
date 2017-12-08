afa38310bf03433477a1a1dda604f65e3f3b1372
ssmylh <ssmylh@gmail.com>
2017-12-08 15:04:30 +0900

    Chapter 9

--- src/main/java/money/Franc.java
+++ src/main/java/money/Franc.java
@@ -1,12 +1,12 @@
 package money;
 
 public class Franc extends Money {
-    Franc(int amount) {
-        this.amount = amount;
+    Franc(int amount, String currency) {
+        super(amount, currency);
     }
 
     @Override
     Money times(int multiplier) {
-        return new Franc(amount * multiplier);
+        return Money.franc(amount * multiplier);
     }
 }
