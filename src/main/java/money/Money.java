2028f8983a75079c62b708affcf146be4c67d42e
ssmylh <ssmylh@gmail.com>
2017-12-08 15:46:01 +0900

    Chapter 11

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -29,10 +29,10 @@ class Money {
     }
 
     static Money dollar(int amount) {
-        return new Dollar(amount, "USD");
+        return new Money(amount, "USD");
     }
 
-    static Franc franc(int amount) {
-        return new Franc(amount, "CHF");
+    static Money franc(int amount) {
+        return new Money(amount, "CHF");
     }
 }
